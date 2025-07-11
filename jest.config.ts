a7d58bb4c0c013300943c621794921255895ccbe
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-07-11 16:53:34 +0900

    feat: setup nextjs

---
+++ jest.config.ts
@@ -0,0 +1,8 @@
+// jest.config.ts
+export default {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1', // optional, if using path aliases
+  },
+};
