1bd6a4777875d95ee6ca2ea9e3fe96612815996d
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-08-14 01:39:43 +0800

    feat: added a gray gradient background for partners view

---
+++ src/features/about-us/containers/partners-view.tsx
@@ -0,0 +1,14 @@
+export default function PartnersView() {
+  return (
+    <>
+      <div className="flex min-h-screen min-w-screen justify-center px-4 py-8 bg-[radial-gradient(circle_at_center,_#f5f5f5_0%,_#d9d9d9_70%,_#b3b3b3_100%)]">
+        <div>
+          <h1 className="text-[#003D6F] text-[52px] font-bold">
+            <span className="text-[#DDB518]">Our</span> Partners
+          </h1>
+        </div>
+        <main></main>
+      </div>
+    </>
+  );
+}
