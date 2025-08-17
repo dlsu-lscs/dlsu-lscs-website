7dcb916d035aff4d126f82da486923ce5b2df22e
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-08-18 00:20:27 +0800

    fix: improve partner cards with responsive layout, aspect ratio fixes, and website links

--- src/features/about-us/containers/partners-view.tsx
+++ src/features/about-us/containers/partners-view.tsx
@@ -7,17 +7,20 @@ export default function PartnersView() {
     <>
       <div className="flex min-h-screen min-w-screen flex-col px-24 py-20 bg-[radial-gradient(circle_at_center,_#f5f5f5_0%,_#d9d9d9_70%,_#b3b3b3_100%)]">
         <div className="flex justify-center">
-          <h1 className="text-[#003D6F] text-[52px] font-bold">
+          <h1 className="text-[#003D6F] text-[52px] font-bold text-center">
             <span className="text-[#DDB518]">Our</span> Partners
           </h1>
         </div>
         {/* List of Partners */}
-        <main className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4 lg:gap-16 mt-16">
-          {partnersData.partners.map((partner: PartnerType, index: number) => {
-            return (
-              <PartnerCard key={index} partnerName={partner.name} partnerLogo={partner.logo} />
-            );
-          })}
+        <main className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8 mt-16 place-items-center">
+          {partnersData.partners.map((partner: PartnerType, index) => (
+            <PartnerCard
+              key={index}
+              partnerName={partner.name}
+              partnerLogo={partner.logo}
+              partnerLink={partner.link}
+            />
+          ))}
         </main>
       </div>
     </>
