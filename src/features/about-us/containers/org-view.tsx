6db12801a4e88178ac9d7df72dccbd034bad2c24
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-08-19 02:11:03 +0800

    feat: added the gray background

---
+++ src/features/about-us/containers/org-view.tsx
@@ -0,0 +1,7 @@
+export default function OrgView() {
+  return (
+    <>
+      <div className="relative z-20 flex min-h-screen min-w-screen flex-col px-24 py-20 bg-[radial-gradient(circle_at_center,_#f5f5f5_0%,_#d9d9d9_70%,_#b3b3b3_100%)]"></div>
+    </>
+  );
+}
