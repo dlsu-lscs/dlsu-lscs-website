2373c86ca650c5b59cc5ca8f1ef408837a93edc5
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-08-19 01:42:02 +0800

    feat: made it responsive

--- src/features/about-us/containers/awards-recognition-view.tsx
+++ src/features/about-us/containers/awards-recognition-view.tsx
@@ -16,8 +16,12 @@ export default function AwardsRecognitionView() {
 
   return (
     <>
-      <div className="relative z-20 min-h-screen min-w-screen bg-gradient-to-b from-[#003D6F] to-[#041019] flex flex-col items-center justify-center">
-        <img src="/puzzle.png" alt="puzzle" className="absolute -top-24 left-0 w-[380px] h-auto" />
+      <div className="relative z-20 min-h-screen bg-gradient-to-b from-[#003D6F] to-[#041019] flex flex-col items-center justify-center">
+        <img
+          src="/puzzle.png"
+          alt="puzzle"
+          className="absolute -top-24 left-0 w-[200px] md:w-[380px] h-auto -z-10 md:z-0"
+        />
         <div
           className="absolute inset-0 bg-[url('/dots.png')] bg-cover bg-left-top min-h-screen opacity-60"
           style={{
@@ -30,23 +34,23 @@ export default function AwardsRecognitionView() {
 
         <div className="flex flex-col gap-10">
           <h1
-            className="text-[#FEE373] font-bold text-5xl text-center"
+            className="text-[#FEE373] font-bold px-2 text-4xl md:text-5xl text-center"
             style={{ textShadow: '3px 3px 7px rgba(0,0,0,0.8)' }}
           >
             Awards <span className="text-[#003D6F]">&</span> Recognition
           </h1>
 
-          <p className="text-center text-[#FFFF] text-lg w-[540px] h-[186px]">
-            The La Salle Computer Society shall serve as a venue for the growth and development of
-            its member through a three-step course of:
+          <p className="text-center text-[#FFFF] text-wrap px-4 text-md md:text-lg">
+            The La Salle Computer Society shall serve as a venue for the growth and
+            <br /> development of its member through a three-step course of:
           </p>
         </div>
-        <main className="w-[94%]">
+        <main className="w-[75%] sm:w-[94%] mt-12">
           {firstHalf.length > 0 ? (
             <Carousel>
-              <CarouselContent className="p-4 -ml-1">
+              <CarouselContent className="py-4">
                 {firstHalf.map((award, index) => (
-                  <CarouselItem key={index} className="basis-1/4 px-2">
+                  <CarouselItem key={index} className="basis-full sm:basis-1/4">
                     <AwardsCard
                       standing={award.standing}
                       awardCommmittee={award.awardCommmittee}
@@ -63,9 +67,9 @@ export default function AwardsRecognitionView() {
           {secondHalf.length > 0 ? (
             <>
               <Carousel>
-                <CarouselContent className="p-4 -ml-1">
+                <CarouselContent className="py-4">
                   {secondHalf.map((award, index) => (
-                    <CarouselItem key={index} className="basis-1/4 px-2">
+                    <CarouselItem key={index} className="basis-full sm:basis-1/4">
                       <AwardsCard
                         standing={award.standing}
                         awardCommmittee={award.awardCommmittee}
