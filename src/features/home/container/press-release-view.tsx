9ea86d4e91463cfaf96912c47d4c8ea4a981e5c6
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-08-18 17:20:54 +0800

    feat: made it rendered as carousel

--- src/features/home/container/press-release-view.tsx
+++ src/features/home/container/press-release-view.tsx
@@ -1,14 +1,38 @@
 import PressReleaseCard from '../components/press-release-card';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel';
+import pressReleasesData from '../data/press-releases.json';
+import { pressRelease as PressReleaseType } from '../types/press-releases';
 
 export default function PressReleaseView() {
   return (
     <>
-      <div className="min-h-screen min-w-screen bg-white">
+      <div className="min-h-screen min-w-screen bg-white border-black border-2">
         <div className="flex justify-center text-center mt-20">
           <h1 className="font-bold text-5xl">PRESS RELEASES</h1>
         </div>
-        <main className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-20">
-          <PressReleaseCard />
+        <main className="w-[95%] px-20 py-24">
+          <Carousel>
+            <CarouselContent>
+              {pressReleasesData.pressReleases.map((pressRelease: PressReleaseType, index) => (
+                <CarouselItem key={index} className="basis-1/4">
+                  <PressReleaseCard
+                    type={pressRelease.type}
+                    title={pressRelease.title}
+                    content={pressRelease.content}
+                    buttonText={pressRelease.buttonText}
+                  />
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
         </main>
       </div>
     </>
