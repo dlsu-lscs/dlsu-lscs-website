09e3871cf6e7e640f43fde3ad63833c3d8df3c4a
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-08-19 01:48:40 +0800

    feat: made more responsive rules

--- src/features/home/container/press-release-view.tsx
+++ src/features/home/container/press-release-view.tsx
@@ -14,13 +14,13 @@ export default function PressReleaseView() {
     <>
       <div className="min-h-screen min-w-screen bg-white border-black border-2">
         <div className="flex justify-center text-center mt-20">
-          <h1 className="font-bold text-5xl">PRESS RELEASES</h1>
+          <h1 className="font-bold text-4xl sm:text-5xl">PRESS RELEASES</h1>
         </div>
         <main className="w-[95%] px-20 py-24">
           <Carousel>
             <CarouselContent>
               {pressReleasesData.pressReleases.map((pressRelease: PressReleaseType, index) => (
-                <CarouselItem key={index} className="md:basis-1/4">
+                <CarouselItem key={index} className="sm:basis-1/4 md:basis-1/2 lg:basis-1/4">
                   <PressReleaseCard
                     type={pressRelease.type}
                     title={pressRelease.title}
