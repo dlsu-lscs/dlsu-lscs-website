0c19b7cbc7d1d1f7a7cc6d6d2e44d42f92cced90
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-08-19 11:06:40 +0800

    feat: added the content callout and golden header

--- src/features/about-us/containers/org-view.tsx
+++ src/features/about-us/containers/org-view.tsx
@@ -4,8 +4,8 @@ import GoldHeader from '../components/gold-header';
 export default function OrgView() {
   return (
     <>
-      <div className="relative z-20 flex min-h-screen min-w-screen flex-col px-24 py-20 bg-[radial-gradient(circle_at_center,_#f5f5f5_0%,_#d9d9d9_70%,_#b3b3b3_100%)]">
-        <div>
+      <div className="relative z-20 flex flex-col items-center min-h-screen min-w-screen px-24 py-20 bg-[radial-gradient(circle_at_center,_#f5f5f5_0%,_#d9d9d9_70%,_#b3b3b3_100%)]">
+        <div className="flex mt-12">
           <div></div>
           <div>
             <GoldHeader>Council of Student Organizations</GoldHeader>
@@ -18,16 +18,14 @@ export default function OrgView() {
             </ContentCallout>
           </div>
         </div>
-        <div>
+        <div className="mt-12">
           <div></div>
           <div>
-            <GoldHeader>Council of Student Organizations</GoldHeader>
+            <GoldHeader>ONECCS</GoldHeader>
             <ContentCallout>
-              The Council of Student Organizations (CSO) is the union of 42 accredited student
-              organizations, categorized as professional, special interest and the socio-civic
-              organizations, of De La Salle University Manila. Since its founding in 1974, the
-              Council continuously delivered quality student services and produced outstanding
-              student leaders dedicated to serving the Lasallian community.
+              ONECCS is the College of Computer Studies Student Council consisting of the University
+              Student Government College and Batch Units, and organizations of the Council of
+              Student Organizations and the office of the Associate Dean.
             </ContentCallout>
           </div>
         </div>
