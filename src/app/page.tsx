3816fa6e8ae31e957f3ec27eb026d3a7a2b78afa
Ellexandrei Esponilla <xandrei37@gmail.com>
2025-08-19 21:20:36 +0800

    Feat/5 who are we view (#22)
    * style: added skeleton of who-are-we page
    
    * style: added bg to who-are-we view, adjusted transparency of bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import LandingPage from '@/features/home/containers/landing-page';
+import WhoAreWe from '@/features/home/containers/who-are-we';
 
 export default function Home() {
   return (
-    <div>
+    <>
       <LandingPage />
-    </div>
+      <WhoAreWe />
+    </>
   );
 }
