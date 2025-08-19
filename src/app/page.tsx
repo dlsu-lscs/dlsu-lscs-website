5df67ab22e876503c75403da0c78e9c34d7fd1ee
Ellexandrei Esponilla <xandrei37@gmail.com>
2025-08-19 21:28:12 +0800

    feat: implemented skeleton of contact us page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import ContactUsPage from '@/features/home/containers/contact-us';
 import LandingPage from '@/features/home/containers/landing-page';
 
 export default function Home() {
   return (
     <div>
       <LandingPage />
+      <ContactUsPage />
     </div>
   );
 }
