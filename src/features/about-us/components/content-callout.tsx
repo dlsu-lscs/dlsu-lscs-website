06343278aeb0bf4dff48aad7cb090d8ee078cebb
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-08-19 23:59:12 +0800

    feat: fixed layout and made it responsive

--- src/features/about-us/components/content-callout.tsx
+++ src/features/about-us/components/content-callout.tsx
@@ -5,8 +5,8 @@ type Props = {
 export default function ContentCallout({ children }: Props) {
   return (
     <>
-      <div className="border-l-4 border-black px-4 w-[40%] mt-6">
-        <p className="w-full break-after-right">{children}</p>
+      <div className="border-l-4 border-black px-4 mt-6">
+        <p className="w-full break-after-right text-md">{children}</p>
       </div>
     </>
   );
--- src/features/about-us/components/gold-header.tsx
+++ src/features/about-us/components/gold-header.tsx
@@ -7,7 +7,7 @@ export default function GoldHeader({ children }: GoldHeaderProps) {
     <>
       <div className="flex">
         <div className="bg-gradient-to-r from-[#FEE373] to-[#DDB518] py-2 px-4 rounded-lg drop-shadow-2xl shadow-xl">
-          <h1 className="bg-gradient-to-r from-[#002D57] to-[#1A5D89] bg-clip-text text-transparent text-center font-bold text-4xl">
+          <h1 className="bg-gradient-to-r from-[#002D57] to-[#1A5D89] bg-clip-text text-transparent text-center font-bold text-3xl md:text-4xl">
             {children}
           </h1>
         </div>
