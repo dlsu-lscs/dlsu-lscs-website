4518075035125ebe2ba21138d6e2d8047b8c0f50
Ellexandrei Esponilla <xandrei37@gmail.com>
2025-08-20 10:45:18 +0800

    feat: added temp gallery, added lscs bt text

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,5 +1,5 @@
 import PartnersView from '@/features/about-us/containers/partners-view';
-import About from '@/features/about-us/section/about';
+import About from '@/features/about-us/containers/about';
 
 export default function AboutUs() {
   return (
