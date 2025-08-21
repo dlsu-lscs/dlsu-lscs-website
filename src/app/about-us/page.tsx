0d660d0dbc82a9c98abc9cf4dabb863d792115d7
Jose Simbillo <144484430+GithubSim13@users.noreply.github.com>
2025-08-21 11:52:03 +0800

    style: moved vision-mission.tsx to container folder

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,6 +1,6 @@
 import PartnersView from '@/features/about-us/containers/partners-view';
 import About from '@/features/about-us/section/about';
-import VisionMission from '@/features/about-us/section/vision-mission';
+import VisionMission from '@/features/about-us/containers/vision-mission';
 
 export default function AboutUs() {
   return (
