fdbe53ef81dda6cd41d36aa04b44839f499edeec
Max Chavez <epikstrykermaxchavez23@gmail.com>
2025-08-22 19:27:09 +0800

    Feat/14 awards recognition view (#20)
    * feat: created the container and added a award component with the title and desc of the container
    
    * feat: added a carousel for the cards and a types and data for the awards
    
    * feat: added circles logo and added the puzzle image to the container
    
    * feat: made it responsive
    
    * feat: fix carousel layout for awards cards
    
    - ensured award cards have proper spacing on tablet size (md)
    - centered award cards on phone size (sm and below)
    - updated CarouselItem basis and flex settings for responsive alignment
    
    * feat: fix spacingbetween the awards-cards
    
    * feat: made it responsive based on the zoom and inches screen of laptop/monitor and for all devices too
    
    ---------
    
    Co-authored-by: Ellexandrei Esponilla <xandrei37@gmail.com>

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,3 +1,4 @@
+import AwardsRecognitionView from '@/features/about-us/containers/awards-recognition-view';
 import OrgView from '@/features/about-us/containers/org-view';
 import PartnersView from '@/features/about-us/containers/partners-view';
 import About from '@/features/about-us/containers/about';
@@ -6,6 +7,7 @@ export default function AboutUs() {
   return (
     <>
       <About />
+      <AwardsRecognitionView />
       <OrgView />
       <PartnersView />
     </>
