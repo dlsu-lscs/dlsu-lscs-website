eada3d45a190fa8e41038dc650ec3bd40284fa75
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-08-27 13:09:45 +0800

    feat: add transition to each views

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,25 @@ import PressReleaseView from '@/features/home/containers/press-release-view';
 import ContactUsPage from '@/features/home/containers/contact-us';
 import LandingPage from '@/features/home/containers/landing-page';
 import WhoAreWe from '@/features/home/containers/who-are-we';
+import ScrollAnimation from '@/components/animation/scroll-animation';
 
 export default function Home() {
   return (
     <>
-      <LandingPage />
-      <WhoAreWe />
-      <PressReleaseView />
-      <ContactUsPage />
+      <div className="h-screen overflow-y-scroll snap-y snap-mandatory">
+        <ScrollAnimation>
+          <LandingPage />
+        </ScrollAnimation>
+        <ScrollAnimation>
+          <WhoAreWe />
+        </ScrollAnimation>
+        <ScrollAnimation>
+          <PressReleaseView />
+        </ScrollAnimation>
+        <ScrollAnimation>
+          <ContactUsPage />
+        </ScrollAnimation>
+      </div>
     </>
   );
 }
