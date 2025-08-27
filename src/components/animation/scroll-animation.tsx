026bb35ce45323bbf0b0302d9ff7b7c5422114cf
mbchavez27 <epikstrykermaxchavez23@gmail.com>
2025-08-27 23:09:17 +0800

    fix: fixed the overshoot of pixels

--- src/components/animation/scroll-animation.tsx
+++ src/components/animation/scroll-animation.tsx
@@ -8,14 +8,15 @@ type ScrollAnimationProps = {
 
 export default function ScrollAnimation({ children }: ScrollAnimationProps) {
   return (
-    <motion.div
-      className="snap-start"
-      initial={{ opacity: 0, y: 50 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.5, ease: 'easeOut' }}
-      viewport={{ amount: 0.2 }}
-    >
-      {children}
-    </motion.div>
+    <div className="snap-start">
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, ease: 'easeOut' }}
+        viewport={{ amount: 0.2 }}
+      >
+        {children}
+      </motion.div>
+    </div>
   );
 }
