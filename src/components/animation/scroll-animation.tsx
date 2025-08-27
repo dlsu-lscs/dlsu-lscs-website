25c8780c2dc9bcc38cb203cf2f9111cf03cb0c1e
Max Chavez <epikstrykermaxchavez23@gmail.com>
2025-08-27 23:12:23 +0800

    Feat/add transitions views (#35)
    * feat: add transition to each views
    
    * feat: installed framer and added a reusable component for the scroll transition
    
    * style: removed the side borders for press-release view
    
    * fix: fixed the overshoot of pixels
    
    ---------
    
    Co-authored-by: Ellexandrei Esponilla <xandrei37@gmail.com>

---
+++ src/components/animation/scroll-animation.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { motion } from 'framer-motion';
+
+type ScrollAnimationProps = {
+  children: React.ReactNode;
+};
+
+export default function ScrollAnimation({ children }: ScrollAnimationProps) {
+  return (
+    <div className="snap-start">
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, ease: 'easeOut' }}
+        viewport={{ amount: 0.2 }}
+      >
+        {children}
+      </motion.div>
+    </div>
+  );
+}
