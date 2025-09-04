7e34fc2927c42a46cb34f70936b92bea6f5bd041
Max Chavez <epikstrykermaxchavez23@gmail.com>
2025-09-04 11:10:14 +0800

    merge to staging: snap transition per view
    * chore: added dockerfile (#33)
    
    * chore: added dockerfile
    
    * fix: next output changed to standalone, and lefthook excluded in docker build
    
    * Feat/add transitions views (#35)
    
    * feat: add transition to each views
    
    * feat: installed framer and added a reusable component for the scroll transition
    
    * style: removed the side borders for press-release view
    
    * fix: fixed the overshoot of pixels
    
    ---------
    
    Co-authored-by: Ellexandrei Esponilla <xandrei37@gmail.com>
    
    ---------
    
    Co-authored-by: Ellexandrei Esponilla <xandrei37@gmail.com>

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -3,15 +3,28 @@ import OrgView from '@/features/about-us/containers/org-view';
 import PartnersView from '@/features/about-us/containers/partners-view';
 import About from '@/features/about-us/containers/about';
 import CoreValuesView from '@/features/about-us/containers/core-values-view';
+import ScrollAnimation from '@/components/animation/scroll-animation';
 
 export default function AboutUs() {
   return (
     <>
-      <About />
-      <CoreValuesView />
-      <AwardsRecognitionView />
-      <OrgView />
-      <PartnersView />
+      <div className="h-screen overflow-y-scroll snap-y snap-mandatory">
+        <ScrollAnimation>
+          <About />
+        </ScrollAnimation>
+        <ScrollAnimation>
+          <CoreValuesView />
+        </ScrollAnimation>
+        <ScrollAnimation>
+          <AwardsRecognitionView />
+        </ScrollAnimation>
+        <ScrollAnimation>
+          <OrgView />
+        </ScrollAnimation>
+        <ScrollAnimation>
+          <PartnersView />
+        </ScrollAnimation>
+      </div>
     </>
   );
 }
